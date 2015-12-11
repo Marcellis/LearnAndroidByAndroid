d9fb29190f115f9d684c8e0f57f82e3b1b78503f
Marcellis <m.m.c.m.marcellis@hva.nl>
2015-12-11 20:45:02 +0100

    Spinner added

--- app/src/main/java/org/mobile_development/marcellis/learnandroidbyandroid/MainActivity.java
+++ app/src/main/java/org/mobile_development/marcellis/learnandroidbyandroid/MainActivity.java
@@ -1,15 +1,27 @@
 package org.mobile_development.marcellis.learnandroidbyandroid;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+
+    EditText editText;
+    Button buttonSnackbar;
+    TextView textView;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +30,36 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        editText = (EditText) findViewById(R.id.editTextSnackbar);
+        buttonSnackbar = (Button) findViewById(R.id.buttonSnackbar);
+        textView = (TextView) findViewById(R.id.textViewOutput);
+        spinner = (Spinner) findViewById(R.id.spinnerSnackbar);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.snackbar_duration, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+        buttonSnackbar.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            public void onClick(View v) {
+                String message = editText.getText().toString();
+
+                Snackbar.make(v, message, Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                textView.setText("Snackbar.make(v," + message + ", Snackbar.LENGTH_LONG)setAction(\"Action\", null).show()");
+
             }
         });
+
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -41,11 +74,11 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
