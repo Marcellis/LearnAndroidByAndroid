06bac790497b93a852b903197e64f221c8987016
Marcellis <m.m.c.m.marcellis@hva.nl>
2015-12-16 21:22:43 +0100

    Action added

--- app/src/main/java/org/mobile_development/marcellis/learnandroidbyandroid/MainActivity.java
+++ app/src/main/java/org/mobile_development/marcellis/learnandroidbyandroid/MainActivity.java
@@ -4,26 +4,24 @@ import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
-    EditText editText;
-    Button buttonSnackbar;
-    TextView textView;
-    Spinner spinner;
-    String clickedItem;
-    int i;
+    private EditText editText;
+    private EditText editTextAction;
+    private Button buttonSnackbar;
+    private TextView textView;
+    private Spinner spinner;
+    private String clickedItem;
+    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,59 +30,59 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //initialize widgets
+
         editText = (EditText) findViewById(R.id.editTextSnackbar);
+        editTextAction = (EditText) findViewById(R.id.editTextAction);
         buttonSnackbar = (Button) findViewById(R.id.buttonSnackbar);
         textView = (TextView) findViewById(R.id.textViewOutput);
         spinner = (Spinner) findViewById(R.id.spinnerSnackbar);
 
-        // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.snackbar_duration, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(this);
-
+        // get selected item of spinner
 
         buttonSnackbar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                clickedItem = String.valueOf(spinner.getSelectedItem());
+
+                switch (clickedItem) {
+                    case "Snackbar.LENGTH_INDEFINITE":
+                        i = Snackbar.LENGTH_INDEFINITE;
+                        break;
+                    case "Snackbar.LENGTH_LONG":
+                        i = Snackbar.LENGTH_LONG;
+                        break;
+                    case "Snackbar.LENGTH_SHORT":
+                        i = Snackbar.LENGTH_SHORT;
+                        break;
+                }
+
                 String message = editText.getText().toString();
+                String action = editTextAction.getText().toString();
 
-                Snackbar.make(v, message, i)
-                        .setAction("Action", null).show();
+                Snackbar snackbar = Snackbar.make(v, message, i);
+                if (!action.equals("")) {
+                    snackbar.setAction(action, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            .setText("This actions change text in message");
+                        }
+                    });
+                }
+                snackbar.show();
 
-                textView.setText("Snackbar.make(v," + message + ","+ clickedItem+" )setAction(\"Action\", null).show()");
+                textView.setText("Snackbar snackbar =  Snackbar.make( View ,\"" + message + "\", " + clickedItem + " );");
+                if (!action.equals("")) {
+                    textView.append("\nsnackbar.setAction(\"" + action + "\")");
+                }
+                textView.append("\nsnackbar.show();");
+
 
             }
         });
     }
 
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        clickedItem = (parent.getItemAtPosition(position)).toString();
-
-        switch (clickedItem) {
-            case "Snackbar.LENGTH_INDEFINITE":
-                i = Snackbar.LENGTH_INDEFINITE;
-                break;
-            case "Snackbar.LENGTH_LONG":
-                i = Snackbar.LENGTH_LONG;
-                break;
-            case "Snackbar.LENGTH_SHORT":
-                i = Snackbar.LENGTH_SHORT;
-                break;
-
-        }
-        }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -98,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+//        int id = item.getItemId();
 //
 //        //noinspection SimplifiableIfStatement
 //        if (id == R.id.action_settings) {
